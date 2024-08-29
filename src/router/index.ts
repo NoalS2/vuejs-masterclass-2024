6c9c5983709d09718b610f793937798d7430512a
Noal <noalssmith@gmail.com>
2024-08-28 20:15:34 -0700

    added file based routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,9 @@
-import HomeView from '@/views/HomeView.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/ProjectsView.vue')
-    }
-  ]
+  routes
 })
 
 export default router
